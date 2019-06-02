ea1a2acf2a9bbf2f61293e4bb5182d39099bf7ba
ANAND <ClobberXD@gmail.com>
2019-06-02 09:55:19 +0530

    MessageFormatter: Move function defs to src/messageformatter.cpp

---
+++ src/messageformatter.cpp
@@ -0,0 +1,62 @@
+﻿#include "messageformatter.h"
+
+#include <IrcMessage>
+#include <QString>
+
+// TODO - Implement functions to format all message types
+QString MessageFormatter::formatMessage(IrcPrivateMessage *msg)
+{
+	assert(!s_format.isEmpty());
+
+	QString message = s_format;
+
+	// Replace name placeholder
+	message.replace(s_msg_pos, 2, msg->nick());
+
+	// Replace message placeholder
+	message.replace(s_name_pos, 2, msg->content());
+
+	// Check if timestamp_pos is valid first, as it's an optional placeholder
+	if (s_timestamp_pos != -1)
+		message.replace(s_timestamp_pos, 2, msg->timeStamp().toString());
+
+	return message;
+}
+
+// Convenience method that allows a format string to be passed with the message itself
+QString MessageFormatter::formatMessage(IrcPrivateMessage *msg, const QString fmt)
+{
+	QString old_format = s_format;
+
+	if (!setFormat(fmt)) {
+		// TODO - log an error
+	}
+
+	QString message = formatMessage(msg);
+
+	// Reset to original format again
+	setFormat(old_format);
+
+	return message;
+}
+
+const QString MessageFormatter::getFormat()
+{
+	return s_format;
+}
+
+bool MessageFormatter::setFormat(QString fmt)
+{
+	s_name_pos = fmt.indexOf("%N");
+	if (s_name_pos == -1)
+		return false;
+
+	s_msg_pos = fmt.indexOf("%M");
+	if (s_msg_pos == -1)
+		return false;
+
+	s_timestamp_pos = fmt.indexOf("%T");
+
+	s_format = fmt;
+	return true;
+}
--- src/messageformatter.h
+++ src/messageformatter.h
@@ -1,8 +1,7 @@
 ﻿#pragma once
 
-#include <IrcMessage>
-
-#include <QString>
+class IrcPrivateMessage;
+class QString;
 
 /*
  * The following are valid placeholders for the message format:
@@ -19,62 +18,14 @@ class MessageFormatter
 {
 public:
 	// TODO - Implement functions to format all message types
-	static QString formatMessage(IrcPrivateMessage *msg)
-	{
-		assert(!s_format.isEmpty());
-
-		QString message = s_format;
-
-		// Replace name placeholder
-		message.replace(s_msg_pos, 2, msg->nick());
-
-		// Replace message placeholder
-		message.replace(s_name_pos, 2, msg->content());
-
-		// Check if timestamp_pos is valid first, as it's an optional placeholder
-		if (s_timestamp_pos != -1)
-			message.replace(s_timestamp_pos, 2, msg->timeStamp().toString());
-
-		return message;
-	}
+	static QString formatMessage(IrcPrivateMessage *msg);
 
 	// Convenience method that allows a format string to be passed with the message itself
-	static QString formatMessage(IrcPrivateMessage *msg, const QString fmt)
-	{
-		QString old_format = s_format;
+	static QString formatMessage(IrcPrivateMessage *msg, const QString fmt);
 
-		if (!setFormat(fmt)) {
-			// TODO - log an error
-		}
+	static const QString getFormat();
 
-		QString message = formatMessage(msg);
-
-		// Reset to original format again
-		setFormat(old_format);
-
-		return message;
-	}
-
-	static const QString getFormat()
-	{
-		return s_format;
-	}
-
-	static bool setFormat(QString fmt)
-	{
-		s_name_pos = fmt.indexOf("%N");
-		if (s_name_pos == -1)
-			return false;
-
-		s_msg_pos = fmt.indexOf("%M");
-		if (s_msg_pos == -1)
-			return false;
-
-		s_timestamp_pos = fmt.indexOf("%T");
-
-		s_format = fmt;
-		return true;
-	}
+	static bool setFormat(QString fmt);
 
 private:
 	// Make ctor private to disallow instantiation
